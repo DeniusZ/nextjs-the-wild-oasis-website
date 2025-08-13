2ab62543f59ee480f6636c2ca4f7db8612a893e2
Deniss Zubkovs <deniss.zubkovs@issuu.com>
2025-08-13 08:50:26 +0200

    add ability to edit reservation

--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -4,6 +4,7 @@ import { revalidatePath } from "next/cache";
 import { auth, signIn, signOut } from "./auth";
 import { supabase } from "./supabase";
 import { getBookings } from "./data-service";
+import { redirect } from "next/navigation";
 
 export const signInAction = async () => {
   await signIn("google", { redirectTo: "/account" });
@@ -35,6 +36,47 @@ export const updateGuest = async (formData) => {
   }
 
   revalidatePath("/account/profile");
+};
+
+export const updateBooking = async (formData) => {
+  const bookingId = Number(formData.get("bookingId"));
+
+  // 1. Authentication
+  const session = await auth();
+  if (!session) throw new Error("You must be logged in");
+
+  // 2. Authorization
+  const guestBookings = await getBookings(session.user.guestId);
+  const guestBookingIds = guestBookings.map((booking) => booking.id);
+
+  if (!guestBookingIds.includes(bookingId))
+    throw new Error("You are not allowed to update this booking");
+
+  // 3. Update data
+  const updatedData = {
+    numGuests: Number(formData.get("numGuests")),
+    observations: formData.get("observations").slice(0, 1000),
+  };
+
+  // 4. Mutation
+  const { error } = await supabase
+    .from("bookings")
+    .update(updatedData)
+    .eq("id", bookingId)
+    .select()
+    .single();
+
+  // 5. Error handling
+  if (error) {
+    throw new Error("Booking could not be updated");
+  }
+
+  // 6. Revalidation
+  revalidatePath(`/account/reservations/edit/${bookingId}`);
+  revalidatePath("/account/reservations");
+
+  // 7. Redirecting
+  redirect("/account/reservations");
 };
 
 export const deleteReservation = async (bookingId) => {
